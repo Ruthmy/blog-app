4a13f289fad26a1697ee7c8265c7a94528d899e6
Ruthmy <ruthmy17@gmail.com>
2023-09-23 15:28:01 -0600

    Updated the migrations files. - Improved the foreign keys to match Rails name conventions.

--- db/migrate/20230923124712_create_likes.rb
+++ db/migrate/20230923124712_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :users, null: false, foreign_key: true
-      t.references :posts, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
---
+++ db/migrate/20230923124809_rename_users_id_column_in_posts.rb
@@ -0,0 +1,5 @@
+class RenameUsersIdColumnInPosts < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :posts, :user_id, :author_id
+  end
+end
