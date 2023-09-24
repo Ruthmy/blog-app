03272aa3a0e66be9b5fa498da522caf3b923d675
Ruthmy <ruthmy17@gmail.com>
2023-09-23 18:29:17 -0600

    Modified 'user.rb' -model- file - Added validations. - Name must not be blank. - PostsCounter must be an integer greater than or equal to zero.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :user_id
   has_many :likes, foreign_key: :user_id
 
-  # validates :name, presence: true
-  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   # Returns the 3 most recent posts
   def post_recent
