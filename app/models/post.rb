0e31b3a5f815a723740ca680fefc4686183c452e
Ruthmy <ruthmy17@gmail.com>
2023-09-23 18:27:54 -0600

    Modified 'post.rb' -model- file - Added the validations - Title must not be blank and not exceed 250 characters. - CommentsCounter must be an integer greater than or equal to zero. - LikesCounter must be an integer greater than or equal to zero.

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,8 +16,11 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
 
-  # validates :title, presence: true
-  # validates :text, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   after_create :update_user_posts_counter
   after_destroy :update_user_posts_counter
