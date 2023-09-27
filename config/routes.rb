ea244457764df7d5b8e114ee7057af7fba69fe00
Ruthmy <ruthmy17@gmail.com>
2023-09-27 17:50:12 -0600

    Modified 'routes.rb' -file- - Created the necessary URL using the Rails Restful aproach

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+
+  resources :users, only: [:show, :index] do
+    resources :posts, only: [:show, :index]
+  end
 end
