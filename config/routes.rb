3231b9147a527afe00aec9f43e3ca3033b46c120
Ruthmy <ruthmy17@gmail.com>
2023-09-30 17:56:49 -0600

    Modified 'routes.rb' -route- - Added pagination param in the post route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:show, :index] do
-    resources :posts, only: [:show, :index]
+    resources :posts, only: [:show, :index] do
+      # Paginate post with kaminari
+      get '/page/:page', action: :index, on: :collection
+    end
   end
 end
