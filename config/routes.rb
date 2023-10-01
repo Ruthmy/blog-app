3a80c478a39ec6c3b1171db365f06d60d4429817
Ruthmy <ruthmy17@gmail.com>
2023-09-30 20:45:46 -0600

    Modified 'routes.rb' -routes- - Craete new routes for new and create in post and comments controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:show, :index] do
-    resources :posts, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :create] do
       # Paginate post with kaminari
       get '/page/:page', action: :index, on: :collection
     end
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
   end
 end
