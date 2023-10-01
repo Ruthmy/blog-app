529bc4a02216906b1b8f90819db4d8406d41dd2c
Ruthmy <ruthmy17@gmail.com>
2023-09-30 21:43:05 -0600

    Modified 'routes.rb' -routes- - Move comments and likes routes the match post instead of users.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :index] do
     resources :posts, only: [:show, :index, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
       # Paginate post with kaminari
       get '/page/:page', action: :index, on: :collection
     end
-    resources :comments, only: [:new, :create]
-    resources :likes, only: [:create]
   end
 end
